7041c0e273672ea90ec08ba34b11bc8bfd1fa1c8
DarenHein <darenhein@hotmail.com>
2024-01-28 15:52:09 -0700

    agregando pooo dart y un readme

--- Poo_dart/19_sobrecarga_resta.dart
+++ Poo_dart/19_sobrecarga_resta.dart
@@ -6,6 +6,14 @@ class Persona{
   Persona(this.nombre , this.edad ); 
 
   operator -(Persona nueva_persona){
-   
+   return Persona("resultado",this.edad - nueva_persona.edad); 
   }
+}
+
+void main(){
+  var persona1 = Persona("Luis" , 12); 
+  var persona2 = Persona("Luis" , 12);
+  var persona3 = persona1 - persona2; 
+  print(persona3.nombre);  
+  print(persona3.edad);  
 }
